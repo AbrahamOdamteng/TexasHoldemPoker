35ed32529d85ad7f13ef10d7baacbae46e20623f
AbrahamOdamteng <abraham.odamteng.developer@gmail.com>
2018-01-09 10:42:19 +0000

    Completed FourOfAKind

--- TexasHoldem/Hands/FourOfAKind.cs
+++ TexasHoldem/Hands/FourOfAKind.cs
@@ -5,23 +5,152 @@ using System.Text;
 using System.Threading.Tasks;
 using TexasHoldem.Deck;
 using TexasHoldem.Interfaces;
+using TexasHoldem.Utilities;
 
 namespace TexasHoldem.Hands
 {
-    class FourOfAKind : IPokerHand
+    class FourOfAKind : BaseHand
     {
-        public HandRanks HandRank => throw new NotImplementedException();
+        public override IEnumerable<Card> Cards { get; }
+        public override HandRanks HandRank => HandRanks.FourOfAKind;
 
-        public IEnumerable<Card> Cards => throw new NotImplementedException();
+        public IEnumerable<Card> Quad { get; }
+        public CardRanks QuadRank { get; }
+        public Card Kicker { get; set; }
 
-        public int CompareTo(IPokerHand other)
+        private FourOfAKind(IEnumerable<Card> quad, Card kicker)
         {
-            throw new NotImplementedException();
+
+            Quad = quad;
+            QuadRank = quad.First().Rank;
+            Kicker = kicker;
+            Cards = quad.Concat(new[] { kicker }).ToArray();
+            var count = Cards.Count();
+            if(count != 5)
+            {
+                throw new ArgumentException($"A FourOfAKind hand consists of 5 cards, {count} given");
+            }
         }
 
-        public bool Equals(IPokerHand other)
+        public static FourOfAKind CreateInstance(IEnumerable<Card> communityCards, IEnumerable<Card> holeCards)
         {
-            throw new NotImplementedException();
+            Utils.Validate(communityCards, holeCards);
+            if (!(GetHighestFourOfAKind(communityCards) is null))
+            {
+                return null;
+            }
+
+            var allCards = communityCards.Concat(holeCards);
+            var fourOfAKindTuple = GetHighestFourOfAKind(allCards);
+            if (fourOfAKindTuple is null)
+            {
+                return null;
+            }
+            return new FourOfAKind(fourOfAKindTuple.Item1, fourOfAKindTuple.Item2);
+            
         }
+
+        public static FourOfAKind CreateInstance(IEnumerable<Card> cards)
+        {
+            var fourOfAKindtuple = GetHighestFourOfAKind(cards);
+
+            if (fourOfAKindtuple is null)
+            {
+                return null;
+            }
+            return new FourOfAKind(fourOfAKindtuple.Item1, fourOfAKindtuple.Item2);
+        }
+
+        static Tuple<IEnumerable<Card>, Card> GetHighestFourOfAKind(IEnumerable<Card> cards)
+        {
+            var quad = cards.GroupBy(c => c.Rank).SingleOrDefault(g => g.Count() == 4);
+
+            if (quad == null) return null;
+
+            var highestKicker = cards.Where(c => !quad.Contains(c)).OrderBy(c => c.Rank).Last();
+
+            return new Tuple<IEnumerable<Card>, Card>(quad, highestKicker);
+        }
+
+
+        #region override of Object Methods
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+
+        #region Implemenation of IComparable
+        public override int CompareTo(IPokerHand other)
+        {
+            if (other is null) return -1;
+            if (HandRank > other.HandRank) return 1;
+            if (HandRank < other.HandRank) return -1;
+
+            var otherFourOfAKind = other as FourOfAKind;
+            if (otherFourOfAKind is null)
+            {
+                throw new ArgumentException("could not covert object to FourOfAKind");
+            }
+
+            if (QuadRank > otherFourOfAKind.QuadRank) return 1;
+            if (QuadRank < otherFourOfAKind.QuadRank) return -1;
+
+            if (Kicker.Rank > otherFourOfAKind.Kicker.Rank) return 1;
+            if (Kicker.Rank < otherFourOfAKind.Kicker.Rank) return -1;
+
+            return 0;
+        }
+        #endregion
+
+
+
+        #region operator overloading
+
+        public static bool operator ==(FourOfAKind fourOfAKind, IPokerHand pokerHand)
+        {
+            if (fourOfAKind is null)
+            {
+                return pokerHand is null ? true : false;
+            }
+            return fourOfAKind.Equals(pokerHand);
+        }
+
+        public static bool operator !=(FourOfAKind fourOfAKind, IPokerHand pokerHand)
+        {
+            if (fourOfAKind is null)
+            {
+                return pokerHand is null ? false : true;
+            }
+            return !fourOfAKind.Equals(pokerHand);
+        }
+
+        public static bool operator >(FourOfAKind fourOfAKind, IPokerHand pokerHand)
+        {
+            return fourOfAKind.CompareTo(pokerHand) == 1;
+        }
+
+        public static bool operator >=(FourOfAKind fourOfAKind, IPokerHand pokerHand)
+        {
+            var res = fourOfAKind.CompareTo(pokerHand);
+            return res == 0 || res == 1;
+        }
+
+        public static bool operator <(FourOfAKind fourOfAKind, IPokerHand pokerHand)
+        {
+            return fourOfAKind.CompareTo(pokerHand) == -1;
+        }
+
+        public static bool operator <=(FourOfAKind fourOfAKind, IPokerHand pokerHand)
+        {
+            var res = fourOfAKind.CompareTo(pokerHand);
+            return res == 0 || res == -1;
+        }
+        #endregion
     }
 }
---
+++ TexasHoldem/UnitTests/FourOfAKindUnitTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TexasHoldem.Utilities;
+using TexasHoldem.Hands;
+namespace TexasHoldem.UnitTests
+{
+    [TestFixture]
+    class FourOfAKindUnitTests
+    {
+
+        [TestCase("4C 4D 4H 4S 5C","5C 5S", "", false, Description = "FourOfAKind in the community cards")]
+        [TestCase("4C 4D 4H 5C 6C", "4S 5H", "4C 4D 4H 4S 6C", true, Description = "Valid FourOfAKind")]
+        public void Test_FourOfAKind_CreateInstance(string strCommunityCards, string strHoleCards, string strFourOfAKind, bool isValid)
+        {
+            var communityCards = Utils.ParseCards(strCommunityCards);
+            var holeCards = Utils.ParseCards(strHoleCards);
+
+            var fourOfAKind = FourOfAKind.CreateInstance(communityCards, holeCards);
+
+            if (isValid)
+            {
+                Assert.NotNull(fourOfAKind);
+                var fourOfAKindCards = Utils.ParseCards(strFourOfAKind);
+                CollectionAssert.AreEquivalent(fourOfAKindCards, fourOfAKind.Cards);
+            }
+            else
+            {
+                Assert.Null(fourOfAKind);
+            }
+        }
+
+        [TestCase("5C 5C 5C 5C 9C", "5C 5C 5C 5C 9C", true)]
+        [TestCase("5C 5C 5C 5C 9C", "6C 6C 6C 6C 9C", false)]
+        public void Test_FourOfAKind_EqualityOperators(string strInputA, string strInputB, bool areEqual)
+        {
+            var cardsA = Utils.ParseCards(strInputA);
+            var cardsB = Utils.ParseCards(strInputB);
+
+            var FourOfAKindOne = FourOfAKind.CreateInstance(cardsA);
+            var FourOfAKindTwo = FourOfAKind.CreateInstance(cardsB);
+
+            Assert.AreEqual(areEqual, FourOfAKindOne.Equals(FourOfAKindTwo));
+            Assert.AreEqual(areEqual, FourOfAKindOne.Equals((object)FourOfAKindTwo));
+            Assert.AreEqual(areEqual, FourOfAKindOne == FourOfAKindTwo);
+            Assert.AreEqual(!areEqual, FourOfAKindOne != FourOfAKindTwo);
+        }
+
+        [TestCase("7S 7S 7S 7S 8H", "7S 7S 7S 7S 8H", 0, Description = "Four-of-a-kind comparable zero test")]
+        [TestCase("7S 7S 7S 7S 8H", "7S 7S 7S 7S 9S", -1, Description = "Four-of-a-kind kicker comparable test ")]
+        [TestCase("7S 7S 7S 7S 9H", "7S 7S 7S 7S 8S", 1, Description = "Four-of-a-kind kicker comparable test inverse")]
+        [TestCase("7S 7S 7S 7S 8H", "8D 8D 8D 8D 9H", -1, Description = "Four-of-a-kind quad comparable test ")]
+        [TestCase("8D 8D 8D 8D 9H", "7S 7S 7S 7S 8H", 1, Description = "Four-of-a-kind quad comparable test inverse")]
+        public void Test_FourOfAKind_ComparableTests(string strInputA, string strInputB, int comp)
+        {
+            var cardsA = Utils.ParseCards(strInputA);
+            var cardsB = Utils.ParseCards(strInputB);
+            var fourOfAKindOne = FourOfAKind.CreateInstance(cardsA);
+            var fourOfAKindTwo = FourOfAKind.CreateInstance(cardsB);
+
+            Assert.AreEqual(comp, fourOfAKindOne.CompareTo(fourOfAKindTwo));
+
+            if (comp == 0)
+            {
+                Assert.IsTrue(fourOfAKindOne >= fourOfAKindTwo);
+                Assert.IsTrue(fourOfAKindOne <= fourOfAKindTwo);
+                Assert.IsFalse(fourOfAKindOne > fourOfAKindTwo);
+                Assert.IsFalse(fourOfAKindOne < fourOfAKindTwo);
+            }
+            else if (comp == 1)
+            {
+                Assert.IsTrue(fourOfAKindOne >= fourOfAKindTwo);
+                Assert.IsFalse(fourOfAKindOne <= fourOfAKindTwo);
+                Assert.IsTrue(fourOfAKindOne > fourOfAKindTwo);
+                Assert.IsFalse(fourOfAKindOne < fourOfAKindTwo);
+            }
+            else if (comp == -1)
+            {
+                Assert.IsFalse(fourOfAKindOne >= fourOfAKindTwo);
+                Assert.IsTrue(fourOfAKindOne <= fourOfAKindTwo);
+                Assert.IsFalse(fourOfAKindOne > fourOfAKindTwo);
+                Assert.IsTrue(fourOfAKindOne < fourOfAKindTwo);
+            }
+            else
+            {
+                throw new ArgumentException("the value of comp can only be -1,0,1");
+            }
+        }
+
+
+
+        [Test]
+        public void Test_FourOfAKind_EqualityOperators_ForNull()
+        {
+            var cards = Utils.ParseCards("7S 7S 7S 7S 8H");
+            var straintFlush = FourOfAKind.CreateInstance(cards);
+
+            Assert.False(straintFlush.Equals(null));
+
+            Assert.True((FourOfAKind)null == (FourOfAKind)null);
+            Assert.False((FourOfAKind)null == straintFlush);
+            Assert.False(straintFlush == (FourOfAKind)null);
+
+            Assert.False((FourOfAKind)null != (FourOfAKind)null);
+            Assert.True((FourOfAKind)null != straintFlush);
+            Assert.True(straintFlush != (FourOfAKind)null);
+        }
+    }
+}
